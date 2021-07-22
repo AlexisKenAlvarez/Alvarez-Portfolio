4eb3445240949cb3f2ea9a499cfc095ed6fb51b5
Amateurz7 <alexisken1432@gmail.com>
2021-07-22 22:44:34 +0800

    Added JS and Parallax

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+gsap.registerPlugin(ScrollTrigger);
 
+const myMedia = window.matchMedia('(min-width: 710px)');
+
+function changeMedia(x) {
+  if (x.matches) {
+    gsap.to(".flying_axie", {
+      scrollTrigger: {
+        trigger: ".box1",
+        start: "top 13%",
+        end: "bottom bottom",
+        scrub: 1
+      },
+      x: -900,
+      y: -750,
+      scale: 0.5
+    })
+  } else {
+    gsap.to(".flying_axie", {
+      scrollTrigger: {
+        trigger: ".box1",
+        start: "160% 80%",
+        end: "250% bottom",
+        scrub: 1
+      },
+      x: -800,
+      y: -750,
+      scale: 0.5
+    });
+  }
+}
+
+
+myMedia.addListener(changeMedia);
+
+changeMedia(myMedia);
